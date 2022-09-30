d29a479171766f69a6682d44c227adbcfb3f9d11
Victorine JACOB <victorinejacob_git@outlook.fr>
2022-09-30 22:16:38 +0100

    un début de quelque chose mais css nul

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const path = require('path');
 const express = require('express');
 
-
 const app = express();
 
-app.get('/', (req,res) => {
-    return res.sendFile('index.html',{root:'.'});
+app.use('',express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+    console.log(`GET /`);
+	return response.sendFile('index.html', { root: '.' });
+});
+
+app.get('/styles/style.css', (request, response) => {
+    console.log(`GET /styles/style.css`);
+	return response.sendFile('style.css', { root: './styles' });
 });
 
 app.get('/auth/discord', (request, response) => {
+    console.log(`GET /auth/discord`);
 	return response.sendFile('ChoosePage.html', { root: './src' });
 });
 
-//PORT
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening at http://localhost:${port}...`));
+app.get('/DovUniversePart', (request, response) => {
+    console.log(`GET /DovUniversePart`);
+	return response.sendFile('DovUniverseMain.html', { root: './src/DovUniversePart' });
+});
+
+app.get('/OvishPart', (request, response) => {
+    console.log(`GET /OvishPart`);
+	return response.sendFile('OvishMain.html', { root: './src/OvishPart' });
+});
+
+app.get('/ShopPart', (request, response) => {
+    console.log(`GET /ShopPart`);
+	return response.sendFile('ShopMain.html', { root: './src/ShopPart' });
+});
+
+const port = '53134';
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
